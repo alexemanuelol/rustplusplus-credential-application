48071f4422c94ccabf295872f926f2f529434ce7
Trophy198 <spsp8426@naver.com>
2024-07-23 02:03:42 +0900

    update externals for push-receiver-v2

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,7 @@ module.exports = {
                 },
             },
             externals: [
-                'push-receiver',
+                'push-receiver-v2',
             ],
         },
     },
